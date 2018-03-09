5d080482281498fcc5fe6fe2b99b5e89565ade3a
Fernando García Hernández <fernandogarciahernandez@iMac-de-Fernando.local>
2018-03-09 04:14:22 +0000

    Initial Commit

---
+++ AlbumFinder2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlbumFinder2
+//
+//  Created by Fernando García Hernández on 9/3/18.
+//  Copyright © 2018 Fernando García Hernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
